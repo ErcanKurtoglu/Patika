60d27b3777eeb9228ab631cc3ebbf2a080953061
ErcanKurtoglu <155419314+ErcanKurtoglu@users.noreply.github.com>
2024-01-06 08:11:00 +0300

    change def

--- Projeler/Flatten.py
+++ Projeler/Flatten.py
@@ -1,22 +1,11 @@
 # Flatten list object
-def flatten_nD(input_list:list):
-    flattened=[]
-    for element in input_list:
-        if isinstance(element,list):
-            a=flatten_nD(element)
-            flattened.extend(a)
-        else:
-            flattened.append(element)
-    return flattened
-
-
 def flatten(input_list:list):
 
     flattened=[]
     
     for element in input_list:
         if isinstance(element,list):
-            a=flatten_nD(element)
+            a=flatten(element)
             flattened.extend(a)
         else:
             flattened.append(element)
