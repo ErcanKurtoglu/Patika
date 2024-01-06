b6fba6174194bcdc4b4f6c15e4d276d00e9736b2
ErcanKurtoglu <155419314+ErcanKurtoglu@users.noreply.github.com>
2024-01-06 08:27:29 +0300

    smooth and add exam

--- Projeler/Flatten.py
+++ Projeler/Flatten.py
@@ -1,4 +1,4 @@
-# Flatten list object
+# 1-Flatten list object
 def flatten(input_list:list):
 
     flattened=[]
@@ -11,3 +11,22 @@ def flatten(input_list:list):
             flattened.append(element)
 
     return flattened
+
+s=[[1,'a',['cat'],2],[[[3]],'dog'],4,5]
+flatten(s)
+
+# 2-Change short
+def smooth(input_list:list):
+    smoothed=[]
+    a=[]
+    for element in input_list[::-1]:
+        if isinstance(element,list):
+            a=smooth(element)
+            smoothed.append(a)
+        else:
+            smoothed.append(element)
+    
+    return smoothed
+
+l=[[1, 2], [3, 4], [5, 6, 7]]
+smooth(l)
